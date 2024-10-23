e53ae0c74f00ea18129590700189a89a804666af
tangxuu <tangxuu@yeah.net>
2024-10-23 16:10:15 +0800

    first

--- autoDeploy.bat
+++
@@ -1,8 +0,0 @@
-call npm docs:build
-cd docs/.vitepress/dist
-
-git init
-git add -A
-git commit -m "auto construct blog"
-
-git push -f https://github.com/JiuYang-cell/JiuYang-cell.github.io.git master:main
